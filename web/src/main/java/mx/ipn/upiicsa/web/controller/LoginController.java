52f580a8cbb012445c96820b9562b5c7ade60771
Annie Islas <ana.islas0609@gmail.com>
2014-12-29 19:16:58 -0600

    coding #11 Se configura controlador para login

---
+++ web/src/main/java/mx/ipn/upiicsa/web/controller/LoginController.java
@@ -0,0 +1,13 @@
+package mx.ipn.upiicsa.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+	
+	
+	
+
+}
