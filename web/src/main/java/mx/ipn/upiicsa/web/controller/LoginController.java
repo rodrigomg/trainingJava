3a2e8ada0c942aea828a7650721ef7a32a4ace00
Annie Islas <ana.islas0609@gmail.com>
2015-01-02 00:03:38 -0600

    coding #11 Se configura Controlador para vista Login y LoginFailed

--- web/src/main/java/mx/ipn/upiicsa/web/controller/LoginController.java
+++ web/src/main/java/mx/ipn/upiicsa/web/controller/LoginController.java
@@ -1,11 +1,26 @@
 package mx.ipn.upiicsa.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+	
+	@RequestMapping("/login")
+	public String inicio(){
+		return "login/login";
+	}	
+	@RequestMapping("/loginfailed")
+	public String loginerror (Model model){
+		model.addAttribute("message","true");
+		return "login/login";
+	}
+	@RequestMapping("/logout")
+	public String logout(){
+		return "login/login";
+	}
 	
 	
 	
