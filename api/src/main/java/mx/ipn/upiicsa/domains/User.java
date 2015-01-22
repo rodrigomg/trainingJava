4ed5d0fa1b883c72cafc7a25231579e5320216d2
Annie Islas <ana.islas0609@gmail.com>
2015-01-22 00:46:13 -0600

    coding #15 Se configura clase domain para la tabla user

--- api/src/main/java/mx/ipn/upiicsa/domains/User.java
+++ api/src/main/java/mx/ipn/upiicsa/domains/User.java
@@ -3,13 +3,87 @@ package mx.ipn.upiicsa.domains;
 public class User {
 	
 	private Long id;
+	private String username;
+	private String password;
+	private int enabled;
+	private String nombre;
+	private String apellidoPaterno;
+	private String apellidoMaterno;
+	private String fechaNacimiento;
+	private String dateCreated;
+	private String dateUpdated;
+	private String sexo;
+	private String telefono;
 
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getEnabled() {
+		return enabled;
+	}
+	public void setEnabled(int enabled) {
+		this.enabled = enabled;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidoPaterno() {
+		return apellidoPaterno;
+	}
+	public void setApellidoPaterno(String apellidoPaterno) {
+		this.apellidoPaterno = apellidoPaterno;
+	}
+	public String getApellidoMaterno() {
+		return apellidoMaterno;
+	}
+	public void setApellidoMaterno(String apellidoMaterno) {
+		this.apellidoMaterno = apellidoMaterno;
+	}
+	public String getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(String fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	public String getDateCreated() {
+		return dateCreated;
+	}
+	public void setDateCreated(String dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+	public String getDateUpdated() {
+		return dateUpdated;
+	}
+	public void setDateUpdated(String dateUpdated) {
+		this.dateUpdated = dateUpdated;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getUsername() {
+		return username;
+	}
+	
 
 }
