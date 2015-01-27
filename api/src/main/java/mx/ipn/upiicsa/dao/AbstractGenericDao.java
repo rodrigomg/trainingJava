e78810b74ac902c19a5d282f2f30db69a0292e81
rmgarcia <rockdrigo.mtz@gmail.com>
2015-01-27 15:19:34 -0600

    coding #19 Se ajusta la definicion de metodos para Dao Generico

--- api/src/main/java/mx/ipn/upiicsa/dao/AbstractGenericDao.java
+++ api/src/main/java/mx/ipn/upiicsa/dao/AbstractGenericDao.java
@@ -1,9 +1,20 @@
 package mx.ipn.upiicsa.dao;
 
 import java.io.Serializable;
+import java.util.List;
 
 public interface AbstractGenericDao<T, PK extends Serializable> {
-	void create(T nexInstance);
+	
+	void create(T newInstance);
+
 	T read(PK id);
 
+	void update(T transientObject);
+
+	void delete(T persistentObject);
+
+	List<T> findAll();
+
+	long count();
+	
 }
