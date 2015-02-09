432d54e19c9627e3797765c6dc80e8b406b493d9
Annie Islas <ana.islas0609@gmail.com>
2015-02-09 15:32:54 -0600

    coding #18  Se heredo comportamiento de AbstractGenericDao

--- api/src/main/java/mx/ipn/upiicsa/dao/UsuarioDao.java
+++ api/src/main/java/mx/ipn/upiicsa/dao/UsuarioDao.java
@@ -2,8 +2,9 @@ package mx.ipn.upiicsa.dao;
 
 import mx.ipn.upiicsa.domains.User;
 
-public interface UsuarioDao {
 
-	void create(User user);
+public interface UsuarioDao extends AbstractGenericDao<User,Long>{
+
+	
 
 }
