f1fb11b200bba4f4f45336491b1d98798eef143b
Annie Islas <ana.islas0609@gmail.com>
2015-02-12 21:46:01 -0600

    coding #20 Se termina la prueba de integracion para userDao

--- core/src/test/java/mx/ipn/upiicsa/test/integration/dao/hibernate/UserDaoHibernateImplIntegrationTest.java
+++ core/src/test/java/mx/ipn/upiicsa/test/integration/dao/hibernate/UserDaoHibernateImplIntegrationTest.java
@@ -4,6 +4,7 @@ import static org.springframework.util.Assert.isTrue;
 import static org.springframework.util.Assert.notNull;
 
 import java.util.Date;
+import java.util.List;
 
 import mx.ipn.upiicsa.dao.UsuarioDao;
 import mx.ipn.upiicsa.domains.User;
@@ -11,6 +12,7 @@ import mx.ipn.upiicsa.domains.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,6 +27,7 @@ public class UserDaoHibernateImplIntegrationTest {
 	static User user = new User();
 	static Long id = 0L;
 	@Test
+	@Rollback(false)
 	public void prueba1Create(){
 		user.setUsername("niki_bonita@ebc.edu.mx");
 		user.setPassword("anniemama");
@@ -44,5 +47,38 @@ public class UserDaoHibernateImplIntegrationTest {
 		System.out.print("CREATE: "+user.getId());
 		
 	}
-
+	@Test
+	public void prueba2Count(){
+		Long count = usuarioDao.count();
+		isTrue(count > 0L);
+		System.out.print("COUNT: " + count);
+	}
+	@Test
+	public void prueba3Read(){
+		user = usuarioDao.read(id);
+		notNull(user);
+		isTrue(user.getId() == id);
+		
+		System.out.print("READ: " + user.getId());
+	}
+	@Test
+	public void prueba4FindAll(){
+		List<User> listUser = usuarioDao.findAll();
+		notNull(listUser);
+		isTrue(!listUser.isEmpty());
+		System.out.print("FINDALL: " + listUser.size());
+	}
+	@Test
+	@Rollback(false)
+	public void prueba5Update(){
+		user.setEnabled(1);
+		usuarioDao.update(user);
+		notNull(user);
+		System.out.print("UPDTAE: " + user.getEnabled());
+	}
+	@Test
+	@Rollback(false)
+	public void prueba6Delete(){
+		usuarioDao.delete(usuarioDao.read(id));
+	}
 }
