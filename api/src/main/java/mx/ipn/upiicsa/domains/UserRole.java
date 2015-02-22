0563dde0f241acfd3100697fa0ca1a53f79dcfbf
Annie Islas <ana.islas0609@gmail.com>
2015-02-22 15:00:00 -0600

    coding #20 Se crea clase dominio para tabla USER_ROLE

---
+++ api/src/main/java/mx/ipn/upiicsa/domains/UserRole.java
@@ -0,0 +1,27 @@
+package mx.ipn.upiicsa.domains;
+
+public class UserRole {
+	private Long id;
+	private Long userId;
+	private String role;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}	
+
+}
