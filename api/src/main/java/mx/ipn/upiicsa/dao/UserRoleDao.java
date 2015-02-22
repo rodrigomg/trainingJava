b0912ec2fec7e0be8544041ca5ca71874e7e4d68
Annie Islas <ana.islas0609@gmail.com>
2015-02-22 16:23:27 -0600

    coding #21 Se crea capa Dao para domain UserRole

---
+++ api/src/main/java/mx/ipn/upiicsa/dao/UserRoleDao.java
@@ -0,0 +1,8 @@
+package mx.ipn.upiicsa.dao;
+
+import mx.ipn.upiicsa.domains.User;
+import mx.ipn.upiicsa.domains.UserRole;
+
+public interface UserRoleDao extends AbstractGenericDao<User,Long>{
+
+}
